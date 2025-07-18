cb012e8f69c83e23b562883cc057cf2db671eed3
kamranikramofficial <kamranikram706@gmail.com>
2025-07-18 12:05:33 -0700

    add the my first react project in the todo

---
+++ mytodo/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/header.jsx'
+import AddTask from './components/addTask.jsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Header/>
+    <AddTask/>
+    </>
+  )
+}
+
+export default App
