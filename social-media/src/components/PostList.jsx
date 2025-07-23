2559475f2074b88a1cf8e31c9ed00ba15e688677
kamranikramofficial <kamranikram706@gmail.com>
2025-07-23 10:03:51 -0700

    I am solve Api Issue And practise branches

--- social-media/src/components/PostList.jsx
+++ social-media/src/components/PostList.jsx
@@ -1,11 +1,22 @@
 import { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../Store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
-  const { postList } = useContext(PostListData);
+  const { postList, addInitialPost } = useContext(PostListData);
+
+const handleOnGetClick = () => {
+  fetch("https://dummyjson.com/posts")
+    .then((res) => res.json())
+    .then((data) => {
+      addInitialPost(data.posts);
+    });
+};
+
   return (
     <>
+      {postList.length === 0 && <WelcomeMessage OnGetClick={handleOnGetClick} />}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
